60a9ff56f5c97588523ee5a35f840c92c644aac0
Mark Berger <mark.berger@globallogic.com>
2022-12-15 00:27:36 +0200

    Update settings to toggle auto-detect schema changes notifications
    - Added toggle auto-detect schema changes notifications

--- airbyte-webapp/src/pages/ConnectionPage/pages/ConnectionItemPage/SchemaUpdateNotifications.tsx
+++ airbyte-webapp/src/pages/ConnectionPage/pages/ConnectionItemPage/SchemaUpdateNotifications.tsx
@@ -1,22 +1,34 @@
 import React from "react";
-import { FormattedMessage } from "react-intl";
+import { FormattedMessage, useIntl } from "react-intl";
 import { useAsyncFn } from "react-use";
 
 import { ControlLabels } from "components";
 import { Card } from "components/ui/Card";
 import { Switch } from "components/ui/Switch";
+import { ToastType } from "components/ui/Toast";
 
 import { useConnectionEditService } from "hooks/services/ConnectionEdit/ConnectionEditService";
+import { useNotificationService } from "hooks/services/Notification";
 
 import styles from "./SchemaUpdateNotifications.module.scss";
 
 export const SchemaUpdateNotifications: React.FC = () => {
+  const { formatMessage } = useIntl();
+  const { registerNotification } = useNotificationService();
   const { connection, updateConnection, connectionUpdating } = useConnectionEditService();
   const [{ loading }, onChange] = useAsyncFn(async (checked: boolean) => {
-    await updateConnection({
-      connectionId: connection.connectionId,
-      notifySchemaChanges: checked,
-    });
+    try {
+      await updateConnection({
+        connectionId: connection.connectionId,
+        notifySchemaChanges: checked,
+      });
+    } catch (e) {
+      registerNotification({
+        id: "connection.schemaUpdateNotifications.error",
+        text: formatMessage({ id: "connection.schemaUpdateNotifications.error" }),
+        type: ToastType.ERROR,
+      });
+    }
   });
 
   return (
